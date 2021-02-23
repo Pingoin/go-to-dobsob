1bf53159a969624ace93dd6063ea85fd0fe2c429
Pingoin <p.drente@gmx.de>
2021-02-23 08:51:42 +0100

    kommunikation mit Stallaris IO

--- software/Treiber/src/driver.ts
+++ software/Treiber/src/driver.ts
@@ -1,35 +1,23 @@
-import { angleCalc, coordinateCalc } from "astronomy-bundle/utils";
-import { createTimeOfInterest } from "astronomy-bundle/time";
-import { createLocation } from "astronomy-bundle/earth";
+import { angleCalc} from "astronomy-bundle/utils";
+import aa from './astronomical-algorithms'
 import * as net from "net";
-import { copyFileSync } from "fs";
-const breite = 53 + 44 / 60 + 16.44 / 3600;
-const länge = 14 + 2 / 60 + 40.92 / 3600;
-
-function rad2deg(rad: number): number {
-  return (rad / Math.PI) * 180;
-}
-
-function deg2rad(deg: number) {
-  return (deg / 180) * Math.PI;
-}
+const latitude = 53 + 44 / 60 + 16.44 / 3600;
+const longitude = 14 + 2 / 60 + 40.92 / 3600;
 export class Driver {
   private port: number;
   private server: net.Server;
   private socket: net.Socket | null = null;
+  private stellariumWorker:NodeJS.Timeout|null=null;
   private istPosition = {
     alt: 0,
     azimuth: 0,
   };
   private sollPosition = {
-    alt: 0,
-    azimuth: 0,
+    dec: 0,
+    ra: 0,
   };
-  private postitionEq = {
-    rightAscension: 0,
-    declination: 0,
-    radiusVector: 0,
-  };
+  private forcePosition=false;
+
   constructor(port: number) {
     this.port = port;
     this.server = new net.Server();
@@ -39,12 +27,14 @@ export class Driver {
       );
     });
     console.log();
+    setInterval(this.writeOnController.bind(this),500);
+    this.stellariumWorker=setInterval(this.sendPosition.bind(this), 2000);
     this.server.on("connection", this.connected.bind(this));
   }
 
   private connected(socket: net.Socket) {
     console.log("A new connection has been established.");
-    setInterval(this.sendPosition.bind(this), 1000, socket);
+
     // Now that a TCP connection has been established, the server can send data to
     // the client by writing to its socket.
     //socket.write('Hello, client.');
@@ -52,82 +42,74 @@ export class Driver {
       this.socket = socket;
     }
 
+    if(this.stellariumWorker==null){
+      this.stellariumWorker=setInterval(this.sendPosition.bind(this), 2000);
+    }
     // The server can also receive data from the client by reading from its socket.
     socket.on("data", this.setSollByStellarium.bind(this));
 
     // When the client requests to end the TCP connection with the server, the server
     // ends the connection.
-    socket.on("end", function () {
-      this.socket = null;
-      console.log("Closing connection with the client");
-    });
+    socket.on("end", this.closeSocket.bind(this));
 
     // Don't forget to catch error, for your own sake.
-    socket.on("error", function (err) {
+    socket.on("error", this.closeSocket.bind(this));
+  }
+  private closeSocket(err?:Error){
+    this.socket.end(()=>this.socket = null)
+    if(this.stellariumWorker!=null)
+    clearTimeout(this.stellariumWorker);
+    this.stellariumWorker=null;
+
+    if(err){
       console.log(`Error: ${err}`);
-    });
+    }else{
+      console.log("Closing connection with the client");
+    }
+  }
+
+  private writeOnController(){
+    const {
+      azimuth,
+      altitude,
+    } = aa.coordinates.transformEquatorialToHorizontal(aa.julianday.getJulianDay(new Date()),longitude,latitude,this.sollPosition.ra,this.sollPosition.dec);
+
+    this.istPosition.alt=altitude;
+    this.istPosition.azimuth=azimuth;
   }
 
   private setSollByStellarium(chunk: Buffer): void {
     console.log(chunk);
-    let RA = (chunk.readUInt32LE(12) / 0x100000000) * 360;
-    RA -= RA >= 180 ? 360 : 0;
-    const DEC = (chunk.readInt32LE(16) / 0x40000000) * 90;
-    const toi = createTimeOfInterest.fromCurrentTime();
-    const location = createLocation(breite, länge);
-    const coords = {
-      rightAscension: RA,
-      declination: DEC,
-      radiusVector: 0,
-    };
-    const T = toi.getJulianCenturiesJ2000();
-    const {
-      azimuth,
-      altitude,
-    } = coordinateCalc.equatorialSpherical2topocentricHorizontal(
-      coords,
-      location,
-      T
-    );
-    this.sollPosition.azimuth = azimuth;
-    this.sollPosition.alt = altitude;
-    const result = {
-      LENGTH: chunk.readInt16LE(0), //0,1
-      TIME: chunk.readBigInt64LE(2), //2,3,4,5,6
-      RA: RA,
-      DEC: DEC,
-      Az: angleCalc.deg2angle(this.sollPosition.azimuth),
-      alt: angleCalc.deg2angle(this.sollPosition.alt),
-    };
-    console.log(result);
-    if (this.socket != null) {
-      const buffer = Buffer.alloc(24, chunk);
-      buffer.writeInt32BE(0, 20);
-      buffer.writeInt16LE(24, 0);
-      this.socket.write(buffer);
-      console.log(chunk);
-      console.log(buffer);
-    }
-    this.istPosition = this.sollPosition;
+    this.sollPosition.ra = (chunk.readUInt32LE(12) / 0x100000000) * 24;
+    this.sollPosition.dec = (chunk.readInt32LE(16) / 0x40000000) * 90;
   }
 
-  private sendPosition(socket: net.Socket): void {
-    const toi = createTimeOfInterest.fromCurrentTime();
-    const T = toi.getJulianCenturiesJ2000();
-    console.log(this.istPosition.alt);
-    const DEC = Math.asin(
-      Math.sin(deg2rad(breite)) * Math.sin(deg2rad(this.istPosition.alt)) -
-        Math.cos(deg2rad(breite)) *
-          Math.cos(deg2rad(this.istPosition.azimuth)) *
-          Math.cos(deg2rad(this.istPosition.alt))
-    );
-    const stundenwinkel = Math.atan2(
-      Math.cos(deg2rad(this.istPosition.azimuth)),
-      Math.sin(deg2rad(breite)) * Math.cos(deg2rad(this.istPosition.azimuth)) +
-        Math.cos(deg2rad(breite)) * Math.tan(deg2rad(this.istPosition.alt))
-    );
-    console.log(angleCalc.deg2angle(DEC));
-    console.log(angleCalc.deg2angle(stundenwinkel));
+  private sendPosition(): void {
+    const jd = aa.julianday.getJulianDay(new Date());
+    
+    const val={
+      altDeg:angleCalc.deg2angle(this.istPosition.alt),
+      latDeg:angleCalc.deg2angle(latitude),
+      longDeg:angleCalc.deg2angle(longitude),
+      azDeg:angleCalc.deg2angle(this.istPosition.azimuth),
+      starH:angleCalc.deg2time(aa.julianday.localSiderealTime(jd,longitude)/24*360)
+    }
+    const eq=aa.coordinates.transformHorizontalToEquatorial(jd,this.istPosition.alt,this.istPosition.azimuth,longitude,latitude);
+    eq.rightAscension-=0.5/3600;
+    eq.rightAscension=Math.max(eq.rightAscension,0);
+    console.log(val);
+    console.log({RA:angleCalc.deg2time( eq.rightAscension/24*360),DEC:angleCalc.deg2angle(eq.declination)});
 
+    const RAraw=Math.round(eq.rightAscension/24* 0x100000000);
+    const DECraw=Math.round(eq.declination/90*0x40000000);
+    if (this.socket != null) {
+      const buffer = Buffer.alloc(24,0);
+      buffer.writeInt16LE(24, 0);
+      buffer.writeUInt32LE(RAraw,12);
+      buffer.writeInt32LE(DECraw,16);
+      for (let index = 0; index < 10; index++) {
+        this.socket.write(buffer);
+      }
+    }
   }
 }
